869a3faaa2679eb160b46ecff9cbe972ebf3554b
Edson Moreno <edson.moreno@pucrs.br>
2022-11-09 17:24:58 -0300

    Elaboração do frontend para listagem de cep e inicio da elaboração do cadastro de CEPs

---
+++ aspnet/dia04/01_mvcCEP/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using _01_mvcCEP.Models;
+using _01_mvcCEP.Services;
+
+namespace _01_mvcCEP.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+    private readonly ICEPServices _localCEPServices;
+
+    public HomeController(ILogger<HomeController> logger, ICEPServices paramCEPService)
+    {
+        _logger = logger;
+        _localCEPServices=paramCEPService;
+    }
+
+    public IActionResult Index()
+    {
+        var ceps = _localCEPServices.listaCEPs();
+        return View(ceps);
+    }
+
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Create(CEPViewModel novoCEP)
+    {
+        _logger.LogInformation($"CEP:{novoCEP.CEP}; UF:{novoCEP.UF}; ");
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
