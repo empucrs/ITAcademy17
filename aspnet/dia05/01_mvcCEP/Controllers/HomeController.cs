efa2e852cc672f4d55cd5a37f09b980667e695de
Edson Moreno <edson.moreno@pucrs.br>
2022-11-10 15:50:06 -0300

    Fechamento da parte de post do site de ceps

---
+++ aspnet/dia05/01_mvcCEP/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using _01_mvcCEP.Models;
+using _01_mvcCEP.Services;
+
+namespace _01_mvcCEP.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+    private readonly ICEPServices _localCEPServices;
+
+    public HomeController(ILogger<HomeController> logger, ICEPServices paramCEPService)
+    {
+        _logger = logger;
+        _localCEPServices=paramCEPService;
+    }
+
+    public IActionResult Index()
+    {
+        var ceps = _localCEPServices.listaCEPs();
+        return View(ceps);
+    }
+
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Create(CEPViewModel novoCEP)
+    {
+        if(ModelState.IsValid){
+            _localCEPServices.cadastraCEPs(novoCEP);
+            return RedirectToAction("Index");
+        }
+
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
